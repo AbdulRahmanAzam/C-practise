c18ebcffe44b9a7a3597fdbb8ba05d4e9d3e65a3
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2023-11-26 03:13:06 +0500

    Update tic tac toe.c

--- PF project/tic tac toe.c
+++ PF project/tic tac toe.c
@@ -23,41 +23,45 @@ bool check(char board[SIZE][SIZE]){
 char winner(char board[SIZE][SIZE]){
     if(board[0] == board[1] && board[1] == board[2]) return board[0];
     if(board[3] == board[4] && board[4] == board[5]) return board[3];  
-    if(board[6] == board[7] && board[7] == board[8]) return board[6]; 
+    if(board[6] == board[7] && board[7] == board[8]) return board[6];
+    for(int i=0;i<3;i++){
+            if(board[0][i]==board[1][i] && board[1][i]==board[2][i] ) return board[0][i];//for column
+            if(board[i][0]==board[i][1] && board[i][1]==board[i][2]) return board[i][0];//for row
+    }
     
-    if(board[0] == board[3] && board[3] == board[6]) return board[0]; 
-    if(board[1] == board[4] && board[4] == board[7]) return board[1]; 
-    if(board[2] == board[5] && board[5] == board[8]) return board[2]; 
-    
-    if(board[0] == board[4] && board[4] == board[8]) return board[0]; 
-    if(board[2] == board[4] && board[4] == board[6]) return board[6]; 
+    //for diagonals
+    if(board[0][2] == board[1][1] && board[1][1] == board[2][0]) return board[0][2]; 
+    if(board[0][0] == board[1][1] && board[1][1] == board[2][2]) return board[0][0]; 
     
     return 0;
     
 }
-bool posVal(char board[SIZE][SIZE], int n){
-    if(board[n-1]!='O' && board[n-1] != 'X') return 1;
+bool pos_Validate(char board[SIZE][SIZE], int row, int col){
+    if(board[row][col] !='O' && board[row][col] != 'X') return 1;
     return 0;
 }
 void multiplayer(char board[SIZE][SIZE]){
-    int player =1;
-    int pos;
+    char player;
+    int move;
+    int row;
+    int col;
     do{
         print_board(board);
-        printf("Turn of player %d, Enter position ",player);
-        scanf("%d",&pos);
+        printf("Turn of player %c, Enter position ",player);
+        scanf("%d",&move);
+        row = (move - 1) / 3;
+        col = (move -1) % 3;
         
-        if(posVal(board,pos)){
-            if(player==1){
-                board[pos-1] = 'X';
-                player++;
-            }else{
-                board[pos-1] = 'O';
-                player--;
+        if(pos_Validate(board,row,col)){
+            if(player == 'X'){
+                board[row][col] = player;
+            }else if(player == 'O'){
+                board[row][col] = player;
             }
              
+            player = (player == 'X') ? 'O' : 'X';
         }else{
-            printf("Enter position again \n");
+            printf("Enter move again \n");
         }
         if(winner(board)!=0) {
             printf("Winner is %c\n", winner(board));
@@ -72,15 +76,14 @@ void hard(char board[SIZE][SIZE]){
     int player =1;
     int pos;
     do{
-        if(player ==1){
+        if(player == 'X'){
             printf("Enter position ");
             scanf("%d", &pos);
-            player++;
-        }else{
-            pos = computer(board);
-            player--;
+        }else if(player == 'O'){
+            pos = minimax(board);
         }
-        if(posVal(board,pos)){
+        
+        if(pos_Validate(board,pos)){
             
         }else{
             printf("Enter position again \n");
@@ -88,9 +91,9 @@ void hard(char board[SIZE][SIZE]){
     }while(1);
 }
 int main(){
-    int difficulty_level; // 
+    int difficulty_level; // if single palyer then difficulty level of the game;
     int game_mode; // to play single or multiplayer 
-    int conti; //to continue
+    int cont; //to continue
     do{
         char board[SIZE][SIZE] = {
             {'1','2','3'},
@@ -108,7 +111,7 @@ int main(){
             printf("Do you want to play easy or hard\n\nEnter 1 for Easy and 0 for Hard\n");
             scanf("%d",&difficulty_level);
             if(difficulty_level){
-                
+                // easy(board);
             }else{
                 hard(board);
             }
@@ -116,7 +119,8 @@ int main(){
         print_board(board);
         
         printf("Do you want to play again, Enter 1 to continue \n");
-        
-    }while();
+        scanf("%d",&cont);
+    }while(cont);
+    
     
 }
